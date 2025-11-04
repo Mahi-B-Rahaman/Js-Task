c29b888d0d068d90cb58e0603c09c1527a5d388a
Mahi B Rahaman <99273214+Mahi-B-Rahaman@users.noreply.github.com>
2025-11-04 12:58:05 +0600

    Create problem 6.js

---
+++ problem 6.js
@@ -0,0 +1,3 @@
+Write a function that finds the top 3 products with the highest average ratings, but only includes products with at least 4 reviews.
+Return an array of objects with product_id, name, and avgRating (sorted by avgRating descending).
+Example output: [{ product_id: "P1003", name: "Running Shoes", avgRating: 4.20 }, ...]
