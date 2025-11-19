f1c243316849316fb25f65dd77e556397a37cfa2
Mahi B Rahaman <99273214+Mahi-B-Rahaman@users.noreply.github.com>
2025-11-19 21:57:58 +0600

    Add feature and overall average price calculation
    Implement function to calculate average prices based on product features.

---
+++ problem .js
@@ -0,0 +1,52 @@
+// Write a function that calculates the average price of products that have a specific feature (e.g., "Water Resistant") 
+// and compares it to the overall average price of all products. Return an object with both averages and the difference.
+// Example output for "Water Resistant": { featureAvg: 165.00, overallAvg: 242.80, difference: -77.80 }
+
+//important things to note: 1 ta commeents 1 bar e check korbe, 2 jodi positive o negative duita thake tahole count korbe na
+
+const data = require('./products.json');
+
+  const feature = "Water Resistant";
+  const result =[{feature}];
+
+  //feature average calculation
+    function calculateFeatureAverages(product, feature) {
+      let featureSum = 0;
+      let featureCount = 0;
+    for (let i = 0; i < product.length; i++) {
+      if (product[i].features.includes(feature)) {
+        featureSum += product[i].price;
+        featureCount += 1;
+      }
+      
+    }
+return featureSum/featureCount;
+  }
+
+
+  //overall average calculation
+    function calculateAverages(product) {
+      let overallSum = 0;
+    for (let i = 0; i < product.length; i++) {
+      
+      overallSum += product[i].price;
+      
+    }
+
+    return overallSum
+  }
+
+
+//overall calculation
+overallSum=calculateAverages(data.products);
+overallAvg=(overallSum/data.products.length).toFixed(2);
+
+//feature calculation
+
+let featureAvg = calculateFeatureAverages(data.products, feature).toFixed(2);
+
+//diff
+let difference = (featureAvg - overallAvg).toFixed(2);
+result.push({featureAvg, overallAvg, difference});
+console.log(result);
+
